b490f8ad54e28420ba786d0b886a138feeae25d8
Steven Anderson <sanderson@bpl.org>
2014-10-10 15:35:58 -0400

    Debug cert error

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ AviProcessor::Application.routes.draw do
   get "processor/byfile"
 
   post "processor/byobject"
+  get "processor/byobject"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
