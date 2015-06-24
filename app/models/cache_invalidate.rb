cd7abb77e61f8b6ac4f05f665b1b59937843af5a
Steven Anderson <sanderson@bpl.org>
2015-06-24 15:56:22 -0400

    Added cache invalidate model

--- app/models/cache_invalidate.rb
+++ app/models/cache_invalidate.rb
@@ -6,13 +6,17 @@ class CacheInvalidate
 
 
     if args.has_key?("file_pid")
-      file = ident_from_pid(args["file_pid"])
-      File.delete(file) if File.exist?file
+      #file = ident_from_pid(args["file_pid"])
+      #File.delete(file) if File.exist?file
+
+
     elsif args.has_key?("object_pid")
       Bplmodels::File.find_in_batches('is_file_of_ssim'=>"info:fedora/#{args["object_pid"]}") do |group|
         group.each { |image_id|
-          file = ident_from_pid(image_id['id'])
-          File.delete(file) if File.exist?file
+          #file = ident_from_pid(image_id['id'])
+          #File.delete(file) if File.exist?file
+          dir = ident_from_pid(image_id['id'])
+          FileUtils.rm_rf(dir) if File.directory?(dir)
         }
       end
     elsif args.has_key?("collection_pid")
@@ -24,7 +28,7 @@ class CacheInvalidate
     if !pid.include?(':')
       pid = CGI::unescape(pid)
     end
-    
+
     ident_hash = Digest::MD5.hexdigest(CGI::escape(pid))
 
     directory = '/home/avi/mapped/loris/jp2/' + pid.split(':').first + '/'
@@ -33,7 +37,7 @@ class CacheInvalidate
       directory = directory + ident_hash[index..index+2] + '/'
     end
 
-    directory = directory + 'loris_cache.jp2'
+    #directory = directory + 'loris_cache.jp2'
 
     return directory
   end
