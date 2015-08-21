633d4458639c9a4fe124268fe31434422863d3b6
Steven Anderson <sanderson@bpl.org>
2015-08-21 15:11:41 -0400

    Attempt to fix no live threads error

--- app/models/derivatives.rb
+++ app/models/derivatives.rb
@@ -35,6 +35,7 @@ class Derivatives
           file_object.characterize #if args[:is_new] == "true"
           file_object.generate_derivatives
           file_object.save
+          sleep(0.1)
         }
       end
 
