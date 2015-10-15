048e2c2e18e025bdfb593259aa71614cb5fe000c
Steven Anderson <sanderson@bpl.org>
2015-10-15 16:58:39 -0400

    Attempt to add posix spawn to fix No Threads Left Error. See: https://goo.gl/3sYHTk

--- config/initializers/minimagick_config.rb
+++ config/initializers/minimagick_config.rb
@@ -1,5 +1,7 @@
 MiniMagick.configure do |config|
   config.cli = :imagemagick
-  config.timeout = 2000
+  #config.timeout = 2000
+  config.timeout = 10000
+  config.shell_api = "posix-spawn"
   config.whiny = false
 end
