90e5672d39bf16872eb136ed73d7afd1dae6f4cf
Steven Anderson <sanderson@bpl.org>
2015-10-27 16:39:14 -0400

    Remove timeout option from MiniMagick

--- config/initializers/minimagick_config.rb
+++ config/initializers/minimagick_config.rb
@@ -1,7 +1,7 @@
 MiniMagick.configure do |config|
   config.cli = :imagemagick
   #config.timeout = 2000
-  config.timeout = 100000
+  #config.timeout = 100000
   config.shell_api = "posix-spawn"
   config.whiny = false
 end
