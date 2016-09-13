45a094dd65a02c1466089d4d50e8416c828911ad
Steven Anderson <sanderson@bpl.org>
2016-09-13 15:49:40 -0400

    Update to not try to parse things like HTML pages

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -2,8 +2,9 @@ class Thumbnail
   @queue = :derivatives
 
   AUDIO_TYPES = ['mp3', 'wav', 'mp4']
-  IMAGE_TYPES = ['jpg', 'tif', 'png']
+  IMAGE_TYPES = ['jpg', 'tif', 'png', 'jp2']
   TEXT_TYPES = ['doc', 'docx', 'pdf']
+  EXCLUDE_TYPES = ['html','mp3', 'wav', 'mp4']
 
 
   #Specific pages: http://stackoverflow.com/questions/2974442/convert-pdf-to-images-using-rmagick-ruby
@@ -28,86 +29,87 @@ class Thumbnail
     @thumbnail_url = nil
     @object = ActiveFedora::Base.find(args["object_pid"]).adapt_to_cmodel
 
-    if args["system_type"] == "omeka" && url_list.present?
+    unless EXCLUDE_TYPES.include?(url.split('.').last)
+      if args["system_type"] == "omeka" && url_list.present?
 
-      #For SAIL, the following url doesn't work in ImageMagick if URL not escaped...
-      url_list.each do |url|
+        #For SAIL, the following url doesn't work in ImageMagick if URL not escaped...
+        url_list.each do |url|
 
-        url = url.gsub('[','%5B').gsub(']','%5D')
+          url = url.gsub('[','%5B').gsub(']','%5D')
 
-        if @thumbnail_url.blank? && !AUDIO_TYPES.include?(url.split('.').last) && !(url.include?('amazonaws') && url.downcase.include?('.mp3')) #http://moakleyarchive.omeka.net/items/show/377 is mp3
-          if TEXT_TYPES.include?(url.split('.').last) || (url.include?('amazonaws') && (url.include?('.pdf') || url.include?('.doc') || url.include?('.docx')))
+          if @thumbnail_url.blank? && !AUDIO_TYPES.include?(url.split('.').last) && !(url.include?('amazonaws') && url.downcase.include?('.mp3')) #http://moakleyarchive.omeka.net/items/show/377 is mp3
+            if TEXT_TYPES.include?(url.split('.').last) || (url.include?('amazonaws') && (url.include?('.pdf') || url.include?('.doc') || url.include?('.docx')))
 
-            if url.include?('.docx') || url.include?('.doc')
-              command = 'soffice -convert-to pdf:writer_pdf_Export --outdir public/data/pdf --headless ' + url
-              result = system(command)
-              if result
-                #FIXME: do more than .docx documents
-                url = 'public/data/pdf/' + url.split('/').last.gsub('.docx', '.pdf')
-              end
-            end
-
-            current_page = -1
-            total_colors = 0
-
-            until total_colors > 1 do
-              current_page = current_page + 1
-
-              if url.include?('amazonaws') #Can't do paging as signature is off then 0.o
-                img = Magick::Image.read(url){
-                  self.quality = 100
-                  self.density = 200
-                }.first
-              else
-                img = Magick::Image.read(url.gsub('.pdf', '.pdf[' + current_page.to_s + ']')){
-                  self.quality = 100
-                  self.density = 200
-                }.first
+              if url.include?('.docx') || url.include?('.doc')
+                command = 'soffice -convert-to pdf:writer_pdf_Export --outdir public/data/pdf --headless ' + url
+                result = system(command)
+                if result
+                  #FIXME: do more than .docx documents
+                  url = 'public/data/pdf/' + url.split('/').last.gsub('.docx', '.pdf')
+                end
               end
 
-              total_colors = img.total_colors
-            end
+              current_page = -1
+              total_colors = 0
 
-            @thumbnail_url = @object.generate_thumbnail_url(config_hash)
-          else
+              until total_colors > 1 do
+                current_page = current_page + 1
 
-            #Amazonaws requires a special token so unable to access thumbnails directly...
-            if url.include?('/original/') & !url.include?('amazonaws')
-              #Note: Second gsub removes the extension and then adds .jpg
-              @thumbnail_url = url.gsub('/original/', '/square_thumbnails/').gsub(/\.\w+$/, '') + '.jpg'
-              img =  Magick::Image.read(@thumbnail_url).first
-            elsif url.include?('/files/') & !url.include?('amazonaws')
-              #Note: Second gsub removes the extension and then adds .jpg
-              @thumbnail_url = url.gsub('/files/', '/square_thumbnails/').gsub(/\.\w+$/, '') + '.jpg'
-              img =  Magick::Image.read(@thumbnail_url).first
-            else
-              #@thumbnail_url = @object.generate_thumbnail_url
-              #FIXME:
+                if url.include?('amazonaws') #Can't do paging as signature is off then 0.o
+                  img = Magick::Image.read(url){
+                    self.quality = 100
+                    self.density = 200
+                  }.first
+                else
+                  img = Magick::Image.read(url.gsub('.pdf', '.pdf[' + current_page.to_s + ']')){
+                    self.quality = 100
+                    self.density = 200
+                  }.first
+                end
+
+                total_colors = img.total_colors
+              end
+
               @thumbnail_url = @object.generate_thumbnail_url(config_hash)
-              img =  Magick::Image.read(url).first
+            else
+
+              #Amazonaws requires a special token so unable to access thumbnails directly...
+              if url.include?('/original/') & !url.include?('amazonaws')
+                #Note: Second gsub removes the extension and then adds .jpg
+                @thumbnail_url = url.gsub('/original/', '/square_thumbnails/').gsub(/\.\w+$/, '') + '.jpg'
+                img =  Magick::Image.read(@thumbnail_url).first
+              elsif url.include?('/files/') & !url.include?('amazonaws')
+                #Note: Second gsub removes the extension and then adds .jpg
+                @thumbnail_url = url.gsub('/files/', '/square_thumbnails/').gsub(/\.\w+$/, '') + '.jpg'
+                img =  Magick::Image.read(@thumbnail_url).first
+              else
+                #@thumbnail_url = @object.generate_thumbnail_url
+                #FIXME:
+                @thumbnail_url = @object.generate_thumbnail_url(config_hash)
+                img =  Magick::Image.read(url).first
+              end
             end
+
+            #This is horrible. But if you don't do this, some PDF files won't come out right at all.
+            #Multiple attempts have failed to fix this but perhaps the bug will be patched in ImageMagick.
+            #To duplicate, one can use the PDF files at: http://libspace.uml.edu/omeka/files/original/7ecb4dc9579b11e2b53ccc2040e58d36.pdf
+            img = Magick::Image.from_blob( img.to_blob { self.format = "jpg" } ).first
+
+            thumb = img.resize_to_fit(300,300)
+
+            @object.thumbnail300.content = thumb.to_blob { self.format = "jpg" }
+            @object.thumbnail300.mimeType = 'image/jpeg'
+            @object.thumbnail300.dsLabel = url.split('/').last.gsub(/\....$/, '')
+
+            @object.descMetadata.insert_location_url(@thumbnail_url, nil, 'preview')
+            @object.add_relationship(:is_image_of, "info:fedora/" + @object.pid)
+            @object.add_relationship(:is_exemplary_image_of, "info:fedora/" + @object.pid)
           end
 
-          #This is horrible. But if you don't do this, some PDF files won't come out right at all.
-          #Multiple attempts have failed to fix this but perhaps the bug will be patched in ImageMagick.
-          #To duplicate, one can use the PDF files at: http://libspace.uml.edu/omeka/files/original/7ecb4dc9579b11e2b53ccc2040e58d36.pdf
-          img = Magick::Image.from_blob( img.to_blob { self.format = "jpg" } ).first
 
-          thumb = img.resize_to_fit(300,300)
-
-          @object.thumbnail300.content = thumb.to_blob { self.format = "jpg" }
-          @object.thumbnail300.mimeType = 'image/jpeg'
-          @object.thumbnail300.dsLabel = url.split('/').last.gsub(/\....$/, '')
-
-          @object.descMetadata.insert_location_url(@thumbnail_url, nil, 'preview')
-          @object.add_relationship(:is_image_of, "info:fedora/" + @object.pid)
-          @object.add_relationship(:is_exemplary_image_of, "info:fedora/" + @object.pid)
         end
 
-
-      end
-
-    elsif args["system_type"] == "generic" && url_list.present?
+      elsif args["system_type"] == "generic" && url_list.present?
 
         max_retry = 2
         sleep_time = 60 # In seconds
@@ -224,63 +226,67 @@ class Thumbnail
 
         end
 
-    elsif args["system_type"] == "wgbh" && url_list.present?
+      elsif args["system_type"] == "wgbh" && url_list.present?
 
-      max_retry = 2
-      sleep_time = 60 # In seconds
-      retry_count = 0
+        max_retry = 2
+        sleep_time = 60 # In seconds
+        retry_count = 0
 
-      current_page = 0
-      total_colors = 0
+        current_page = 0
+        total_colors = 0
 
-      url = url_list.first
+        url = url_list.first
 
-      img = nil
+        img = nil
 
-      if @thumbnail_url.blank? && !AUDIO_TYPES.include?(url.split('.').last)
+        if @thumbnail_url.blank? && !AUDIO_TYPES.include?(url.split('.').last)
 
-        response = ::Typhoeus::Request.get(
-            url,
-            method: :get,
-            headers: {'Referer' => 'http://openvault.wgbh.org/' }
-        )
+          response = ::Typhoeus::Request.get(
+              url,
+              method: :get,
+              headers: {'Referer' => 'http://openvault.wgbh.org/' }
+          )
 
-        img =  Magick::Image.from_blob(response.body).first
+          img =  Magick::Image.from_blob(response.body).first
 
-        #Fix for transparency
-        if img.matte
-          img_list = Magick::ImageList.new
-          img_list << img
-          img_list.new_image(img_list.first.columns, img_list.first.rows) { self.background_color = "white" } # Create new "layer" with white background and size of original image
-          img = img_list.reverse.flatten_images
-          img = img.quantum_operator(Magick::MultiplyQuantumOperator, 0.3, Magick::AlphaChannel)
+          #Fix for transparency
+          if img.matte
+            img_list = Magick::ImageList.new
+            img_list << img
+            img_list.new_image(img_list.first.columns, img_list.first.rows) { self.background_color = "white" } # Create new "layer" with white background and size of original image
+            img = img_list.reverse.flatten_images
+            img = img.quantum_operator(Magick::MultiplyQuantumOperator, 0.3, Magick::AlphaChannel)
+          end
+
+          #This is horrible. But if you don't do this, some PDF files won't come out right at all.
+          #Multiple attempts have failed to fix this but perhaps the bug will be patched in ImageMagick.
+          #To duplicate, one can use the PDF files at: http://libspace.uml.edu/omeka/files/original/7ecb4dc9579b11e2b53ccc2040e58d36.pdf
+          img = Magick::Image.from_blob( img.to_blob { self.format = "jpg" } ).first
+
+          thumb = img.resize_to_fit(300,300)
+
+
+          @object.thumbnail300.content = thumb.to_blob { self.format = "jpg" }
+          @object.thumbnail300.mimeType = 'image/jpeg'
+          @object.thumbnail300.dsLabel = url.split('/').last.gsub(/\....$/, '')
+
+          #@thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url #Generate the thumbnail url
+          #FIXME: Temporary... no ARK Config...
+
+          @thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url(config_hash)
+          @object.descMetadata.insert_location_url(@thumbnail_url, 'preview', nil)
+          @object.add_relationship(:is_image_of, "info:fedora/" + @object.pid)
+          @object.add_relationship(:is_exemplary_image_of, "info:fedora/" + @object.pid)
+
+
+
         end
-
-        #This is horrible. But if you don't do this, some PDF files won't come out right at all.
-        #Multiple attempts have failed to fix this but perhaps the bug will be patched in ImageMagick.
-        #To duplicate, one can use the PDF files at: http://libspace.uml.edu/omeka/files/original/7ecb4dc9579b11e2b53ccc2040e58d36.pdf
-        img = Magick::Image.from_blob( img.to_blob { self.format = "jpg" } ).first
-
-        thumb = img.resize_to_fit(300,300)
-
-
-        @object.thumbnail300.content = thumb.to_blob { self.format = "jpg" }
-        @object.thumbnail300.mimeType = 'image/jpeg'
-        @object.thumbnail300.dsLabel = url.split('/').last.gsub(/\....$/, '')
-
-        #@thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url #Generate the thumbnail url
-        #FIXME: Temporary... no ARK Config...
-
-        @thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url(config_hash)
-        @object.descMetadata.insert_location_url(@thumbnail_url, 'preview', nil)
-        @object.add_relationship(:is_image_of, "info:fedora/" + @object.pid)
-        @object.add_relationship(:is_exemplary_image_of, "info:fedora/" + @object.pid)
-
-
 
       end
 
-  end
+    end
+
+
 
   if @object.workflowMetadata.item_status.processing != ["complete"]
     @object.workflowMetadata.item_status.processing = "complete"
