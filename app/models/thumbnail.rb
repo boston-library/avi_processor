5b56e357edb9f4a9b637b16088e5a808a6e8cafb
Steven Anderson <sanderson@bpl.org>
2016-09-14 10:58:21 -0400

    Fix the exclude type logic a tad... this all needs to be refactored...

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -109,7 +109,7 @@ class Thumbnail
 
       end
 
-    elsif args["system_type"] == "generic" && url_list.present? && !EXCLUDE_TYPES.include?(url.split('.').last)
+    elsif args["system_type"] == "generic" && url_list.present?
 
       max_retry = 2
       sleep_time = 60 # In seconds
@@ -122,107 +122,109 @@ class Thumbnail
 
       img = nil
 
-      if @thumbnail_url.blank? && !AUDIO_TYPES.include?(url.split('.').last)
-        if TEXT_TYPES.include?(url.split('.').last)
+      unless EXCLUDE_TYPES.include?(url.split('.').last)
 
-          #Convert docx and doc to PDF
-          if url.include?('.docx') || url.include?('.doc')
-            command = 'soffice -convert-to pdf:writer_pdf_Export --outdir public/data/pdf --headless ' + url
-            result = system(command)
-            if result
-              #FIXME: do more than .docx documents
-              url = 'public/data/pdf/' + url.split('/').last.gsub('.docx', '.pdf')
-            end
-          end
+        if @thumbnail_url.blank? && !AUDIO_TYPES.include?(url.split('.').last)
+          if TEXT_TYPES.include?(url.split('.').last)
 
-          until total_colors > 1 do
-
-
-            begin
-              if retry_count > 0
-                sleep(sleep_time)
+            #Convert docx and doc to PDF
+            if url.include?('.docx') || url.include?('.doc')
+              command = 'soffice -convert-to pdf:writer_pdf_Export --outdir public/data/pdf --headless ' + url
+              result = system(command)
+              if result
+                #FIXME: do more than .docx documents
+                url = 'public/data/pdf/' + url.split('/').last.gsub('.docx', '.pdf')
               end
-              retry_count = retry_count + 1
-              img = Magick::Image.read(url + '[' + current_page.to_s + ']'){
-                self.quality = 100
-                self.density = 200
-              }.first
-            rescue
-              retry if retry_count <= max_retry
-              #raise "Image Magick Error With URL: #{url}"       #No errors - just skip as some PDFs don't exist
-              return false
             end
 
-            total_colors = img.total_colors
-            current_page = current_page + 1
+            until total_colors > 1 do
 
-          end
 
-        else
-
-          until total_colors > 1 do
-
-            begin
-              if retry_count > 0
-                sleep(sleep_time)
+              begin
+                if retry_count > 0
+                  sleep(sleep_time)
+                end
+                retry_count = retry_count + 1
+                img = Magick::Image.read(url + '[' + current_page.to_s + ']'){
+                  self.quality = 100
+                  self.density = 200
+                }.first
+              rescue
+                retry if retry_count <= max_retry
+                #raise "Image Magick Error With URL: #{url}"       #No errors - just skip as some PDFs don't exist
+                return false
               end
-              retry_count = retry_count + 1
-              img =  Magick::Image.read(url).first
-            rescue => error
-              retry if retry_count <= max_retry
-              #raise "Image Magick Error With URL: #{url} and message header #{$!}", $!.backtrace
-              current_error = "Image Magick Error With URL: #{url}\n"
-              current_error += "Error message: #{error.message}\n"
-              current_error += "Error backtrace: #{error.backtrace}\n"
 
-              #Resque.logger.error current_error
+              total_colors = img.total_colors
+              current_page = current_page + 1
 
-              raise current_error
             end
 
-            total_colors = img.total_colors
+          else
 
-            current_page = current_page + 1
+            until total_colors > 1 do
 
-            if current_page > url_list.size
-              raise "Could not find a page that was not blank with url_list: #{url_list}"
+              begin
+                if retry_count > 0
+                  sleep(sleep_time)
+                end
+                retry_count = retry_count + 1
+                img =  Magick::Image.read(url).first
+              rescue => error
+                retry if retry_count <= max_retry
+                #raise "Image Magick Error With URL: #{url} and message header #{$!}", $!.backtrace
+                current_error = "Image Magick Error With URL: #{url}\n"
+                current_error += "Error message: #{error.message}\n"
+                current_error += "Error backtrace: #{error.backtrace}\n"
+
+                #Resque.logger.error current_error
+
+                raise current_error
+              end
+
+              total_colors = img.total_colors
+
+              current_page = current_page + 1
+
+              if current_page > url_list.size
+                raise "Could not find a page that was not blank with url_list: #{url_list}"
+              end
+
+              url = url_list[current_page] unless total_colors > 1 #FIXME: Should be more consistent with the until condition....
             end
 
-            url = url_list[current_page] unless total_colors > 1 #FIXME: Should be more consistent with the until condition....
           end
+
+          #Fix for transparency
+          if img.matte
+            img_list = Magick::ImageList.new
+            img_list << img
+            img_list.new_image(img_list.first.columns, img_list.first.rows) { self.background_color = "white" } # Create new "layer" with white background and size of original image
+            img = img_list.reverse.flatten_images
+            img = img.quantum_operator(Magick::MultiplyQuantumOperator, 0.3, Magick::AlphaChannel)
+          end
+
+          #This is horrible. But if you don't do this, some PDF files won't come out right at all.
+          #Multiple attempts have failed to fix this but perhaps the bug will be patched in ImageMagick.
+          #To duplicate, one can use the PDF files at: http://libspace.uml.edu/omeka/files/original/7ecb4dc9579b11e2b53ccc2040e58d36.pdf
+          img = Magick::Image.from_blob( img.to_blob { self.format = "jpg" } ).first
+
+          thumb = img.resize_to_fit(300,300)
+
+
+          @object.thumbnail300.content = thumb.to_blob { self.format = "jpg" }
+          @object.thumbnail300.mimeType = 'image/jpeg'
+          @object.thumbnail300.dsLabel = url.split('/').last.gsub(/\....$/, '')
+
+          #@thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url #Generate the thumbnail url
+          #FIXME: Temporary... no ARK Config...
+
+          @thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url(config_hash)
+          @object.descMetadata.insert_location_url(@thumbnail_url, 'preview', nil)
+          @object.add_relationship(:is_image_of, "info:fedora/" + @object.pid)
+          @object.add_relationship(:is_exemplary_image_of, "info:fedora/" + @object.pid)
 
         end
-
-        #Fix for transparency
-        if img.matte
-          img_list = Magick::ImageList.new
-          img_list << img
-          img_list.new_image(img_list.first.columns, img_list.first.rows) { self.background_color = "white" } # Create new "layer" with white background and size of original image
-          img = img_list.reverse.flatten_images
-          img = img.quantum_operator(Magick::MultiplyQuantumOperator, 0.3, Magick::AlphaChannel)
-        end
-
-        #This is horrible. But if you don't do this, some PDF files won't come out right at all.
-        #Multiple attempts have failed to fix this but perhaps the bug will be patched in ImageMagick.
-        #To duplicate, one can use the PDF files at: http://libspace.uml.edu/omeka/files/original/7ecb4dc9579b11e2b53ccc2040e58d36.pdf
-        img = Magick::Image.from_blob( img.to_blob { self.format = "jpg" } ).first
-
-        thumb = img.resize_to_fit(300,300)
-
-
-        @object.thumbnail300.content = thumb.to_blob { self.format = "jpg" }
-        @object.thumbnail300.mimeType = 'image/jpeg'
-        @object.thumbnail300.dsLabel = url.split('/').last.gsub(/\....$/, '')
-
-        #@thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url #Generate the thumbnail url
-        #FIXME: Temporary... no ARK Config...
-
-        @thumbnail_url = args["thumbnail_url"].present? ? args["thumbnail_url"] : @object.generate_thumbnail_url(config_hash)
-        @object.descMetadata.insert_location_url(@thumbnail_url, 'preview', nil)
-        @object.add_relationship(:is_image_of, "info:fedora/" + @object.pid)
-        @object.add_relationship(:is_exemplary_image_of, "info:fedora/" + @object.pid)
-
-
 
       end
 
