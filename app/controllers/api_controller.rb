4604c506ee4ee8347334f9dea08f56f9377c2539
Steven Anderson <sanderson@bpl.org>
2016-12-13 15:09:02 -0500

    Part of the GeoTIFF api. Add a checksum as a require param. Check the authentication key first. Fix up the code a bit

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,71 +5,92 @@ class ApiController < ActionController::Base
   end
 
   def geo_tiff_create
-    errors = ''
-    image_solr_response = []
-    datastream = 'geoEncodedMaster'
-
-    expected_params = ["object_id", "image_id", "bbox", "token", "environment"]
-    expected_params.each do |key|
-      if !params.keys.include? key
-        errors += "Missing required key of: #{key.to_s}\n"
+    #Check token first... don't allow anything else if authentication isn't valid.
+    if params["token"].blank? || params["token"] != token
+      respond_to do |format|
+        format.html { render text: "Token is invalid. \n", status: 400 }
       end
-    end
+    else
+      errors = ''
+      image_solr_response = []
+      datastream = 'geoEncodedMaster'
 
-    if params["token"] != token
-      errors += "Token is invalid. \n"
-    end
-
-    begin
-      ActiveFedora.init(:environment=>params["environment"])
-    rescue
-      errors += "Environment value is invalid. \n"
-    end
-
-    begin
-      image_solr_response = Bplmodels::Finder.getImageFiles(params["object_id"]).first["id"]
-      errors += "No Map Object Found for: #{params["object_id"]} \n" if image_solr_response.blank?
-    rescue
-      errors += "Problem loading images for: #{params["object_id"]} \n"
-    end
-
-    begin
-      errors += "Likely a bad geo tiff sent... very little data sent. \n" if request.body.read.size < 100000
-    rescue
-      errors += "Couldn't read the request body... did you send the geo tiff data correctly? \n"
-    end
-
-
-
-    if errors.blank?
-      obj = ActiveFedora::Base.find(params["object_id"]).adapt_to_cmodel
-      image_obj = ActiveFedora::Base.find(image_solr_response[0]["id"]).adapt_to_cmodel
-
-      image_obj.send(datastream).content = request.body.read
-      image_obj.save
-
-      subject_index = obj.descMetadata.mods(0).subject.count
-      0.upto subject_index-1 do |pos|
-        if obj.descMetadata.mods(0).subject(pos).geographic.blank? and obj.descMetadata.mods(0).subject(pos).authority.blank? and obj.descMetadata.mods(0).subject(pos).cartographics(0).coordinates.present?
-          subject_index = pos
-          break
+      expected_params = ["object_id", "image_id", "bbox", "token", "environment", "sha256hex"]
+      expected_params.each do |key|
+        if !params.keys.include? key
+          errors += "Missing required key of: #{key.to_s}\n"
         end
       end
 
-      obj.descMetadata.mods(0).subject(subject_index).cartographics(0).coordinates = params["bbox"]
-      obj.save
+      begin
+        ActiveFedora.init(:environment=>params["environment"])
+      rescue
+        errors += "Environment value is invalid. \n"
+      end
+
+      begin
+        image_solr_response = Bplmodels::Finder.getImageFiles(params["object_id"]).first["id"]
+        if image_solr_response.blank?
+          errors += "No Images Found for map object: #{params["object_id"]} \n"
+        else
+          existing_check = image_solr_response.select { |row| row["id"] == params["image_id"] }
+          errors += "Passed Image ID of: #{params["image_id"]} not found related to Map Object #{params["object_id"]} \n" if existing_check.blank?
+        end
+
+
+
+      rescue
+        errors += "Problem loading images for: #{params["object_id"]} \n"
+      end
+
+      begin
+        errors += "Likely a bad geo tiff sent... very little data sent. \n" if request.body.read.size < 100000
+        errors += "SHA 256 hexdigest did not match the content received. \n" unless Digest::SHA256.hexdigest(request.body.read) == params['sha256hex']
+      rescue
+        errors += "Couldn't read the request body... did you send the geo tiff data correctly? \n"
+      end
+
+
+
+      if errors.blank?
+        begin
+          obj = ActiveFedora::Base.find(params["object_id"]).adapt_to_cmodel
+          image_obj = ActiveFedora::Base.find(params["image_id"]).adapt_to_cmodel
+
+          image_obj.send(datastream).content = request.body.read
+          image_obj.save
+
+          subject_index = obj.descMetadata.mods(0).subject.count
+          0.upto subject_index-1 do |pos|
+            if obj.descMetadata.mods(0).subject(pos).geographic.blank? and obj.descMetadata.mods(0).subject(pos).authority.blank? and obj.descMetadata.mods(0).subject(pos).cartographics(0).coordinates.present?
+              subject_index = pos
+              break
+            end
+          end
+
+          obj.descMetadata.mods(0).subject(subject_index).cartographics(0).coordinates = params["bbox"]
+          obj.save
+        rescue => error
+          errors += "Problem saving the image or coordinates. \n"
+          errors += "Error message: #{error.message}\n"
+          errors += "Error backtrace: #{error.backtrace}\n"
+        end
+      end
+
+      if errors.present?
+        respond_to do |format|
+          format.html { render text: errors, status: 400 }
+        end
+      else
+        respond_to do |format|
+          format.html { render text: "Successfully added GeoTIFF for #{params["object_id"]}" }
+
+        end
+      end
     end
 
-    if errors.present?
-      respond_to do |format|
-        format.html { render text: errors, status: 400 }
-      end
-    else
-      respond_to do |format|
-        format.html { render text: "Successfully added GeoTIFF for #{params["object_id"]}" }
 
-      end
-    end
+
 
   end
 
